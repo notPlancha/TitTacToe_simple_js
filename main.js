cd152e5ef3bba698a15b4cda8406383940d9109e
notPlancha <andre.plancha@hotmail.com>
2021-05-28 23:21:23 +0100

    first commit and design almost done

---
+++ main.js
@@ -0,0 +1,34 @@
+var circle = document.createElement("div")
+  .innerHTML =
+    `
+    <svg viewBox="0 0 80 80" style="height: 80px; width: 80px;">
+      <circle fill="rgba(0,0,0,0)" stroke="black" stroke-width="3"
+stroke-miterlimit="10" cx="40" cy="40" r="20"/>
+    </svg>
+    `
+;
+
+var x = document.createElement("div")
+  .innerHTML =
+  `
+  <svg viewBox="0 0 80 80" style="height: 80px; width: 80px;">
+    <line stroke="black" x1 ="20" y1="20" x2="60" y2="60" stroke-width="3"/>
+    <line stroke="black" x1 ="20" y1="60" x2="60" y2="20" stroke-width="3"/>
+  </svg>
+  `
+
+
+$(".play-area").ready(function(){
+  for (var i = 1; i <= 9; i++) {
+    var $newDiv = $(document.createElement('div'))
+      .addClass("box")
+      .attr("id", "box" + i.toString())
+      .attr("style", "grid-area: box" + i.toString() + ";")
+      .append(x)
+
+    ;
+
+    $(".play-area").append($newDiv)
+
+  }
+})
